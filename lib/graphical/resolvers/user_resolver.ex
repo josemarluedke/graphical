588582dd40838af9acd959ce209af2f24fd784f5
Josemar Luedke <josemarluedke@gmail.com>
2018-09-14 18:02:24 -0700

    Add GraphQL working example

---
+++ lib/graphical/resolvers/user_resolver.ex
@@ -0,0 +1,17 @@
+defmodule Graphical.UserResolver do
+  alias Graphical.Accounts
+  alias Graphical.Repo
+
+  def all(_args, _info) do
+    {:ok, Accounts.list_users()}
+  end
+
+  def find(%{id: id}, _info) do
+    case Repo.get(Accounts.User, id) do
+    # case Accounts.get_user!(id) do
+      nil -> { :error, "User id #{id} not found" }
+      user -> { :ok, user }
+    end
+
+  end
+end
