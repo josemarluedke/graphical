c871fccf006e25932cd9d0914d1c703a43d36c1e
Josemar Luedke <josemarluedke@gmail.com>
2018-09-14 21:02:42 -0700

    Add crud operations for post

--- lib/graphical_web/schema.ex
+++ lib/graphical_web/schema.ex
@@ -18,4 +18,33 @@ defmodule GraphicalWeb.Schema do
     end
   end
 
+  input_object :update_post_params do
+    field :title, non_null(:string)
+    field :body, non_null(:string)
+    field :user_id, non_null(:integer)
+  end
+
+  mutation do
+    field :create_post, type: :post do
+      arg :title, non_null(:string)
+      arg :body, non_null(:string)
+      arg :user_id, non_null(:integer)
+
+      resolve &Graphical.PostResolver.create/2
+    end
+
+    field :update_post,type: :post do
+      arg :id, non_null(:integer)
+      arg :post, :update_post_params
+
+      resolve &Graphical.PostResolver.update/2
+    end
+
+    field :delete_post, type: :post do
+      arg :id, non_null(:integer)
+
+      resolve &Graphical.PostResolver.delete/2
+    end
+  end
+
 end
