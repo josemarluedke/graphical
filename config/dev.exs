9d8bd50be80dbde0cee17ffb707ed70d80df93bc
Josemar Luedke <josemarluedke@gmail.com>
2018-09-14 17:18:52 -0700

    Change db config

--- config/dev.exs
+++ config/dev.exs
@@ -39,8 +39,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :graphical, Graphical.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "neighbor",
+  password: "pgpassword",
   database: "graphical_dev",
   hostname: "localhost",
   pool_size: 10
